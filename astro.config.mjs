de5ff13759fbb23d7d791e10848f66c5e757de76
Andrew Emelianenko <emelianenko.web@gmail.com>
2023-04-01 19:26:17 -0400

    Add GoogleAnalytics and Mailchimp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,22 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
-
 import mdx from "@astrojs/mdx";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    tailwind(),
     mdx(),
+    tailwind(),
     image({
       serviceEntryPoint: '@astrojs/image/sharp'
-    })
+    }),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"]
+      },
+    }),
   ]
 });
