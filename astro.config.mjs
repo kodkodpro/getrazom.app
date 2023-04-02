da512f52e1344ca846e204abac22048023a76680
Andrew Emelianenko <emelianenko.web@gmail.com>
2023-04-02 15:30:44 -0400

    Add Ukrainian language

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
 import image from "@astrojs/image"
 import mdx from "@astrojs/mdx"
+import i18next from "astro-i18next"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     mdx(),
+    i18next(),
     tailwind(),
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
